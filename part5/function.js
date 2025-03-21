be0cd17531e3a60bd92eb69d71010f60b891b1a4
Wahab1437 <wahab.suit997@gmail.com>
2025-03-22 00:22:35 +0500

    some tasks of udemy course have been completed at end

--- part5/function.js
+++ part5/function.js
@@ -79,5 +79,59 @@ function stringToNumber(input) {
     return "Not a number";
   }
 }
+
+//playing with variables
 let num = stringToNumber("123");
-console.log(num); // Not a number
+// console.log(num); // Not a number
+
+const a = 18;
+const b = 24;
+// let sum = a + b;
+function add(a, b) {
+  return a + b;
+}
+let sum = add(a, b);
+console.log(sum); // 42
+
+//subtraction
+const c = 18;
+const d = 24;
+function subtract(c, d) {
+  return d - c;
+}
+let result = subtract(c, d);
+console.log(result);
+
+//division
+const e = 18;
+const f = 24;
+function divide(e, f) {
+  return f / e;
+}
+let division = divide(e, f);
+console.log(division);
+
+// increment
+let i = 19;
+function increment(i) {
+  return i + 1;
+}
+let incrementResult = increment(i);
+console.log(incrementResult);
+
+// decrement
+let j = 24;
+function decrement(j) {
+  return j - 1;
+}
+let decrementResult = decrement(j); // 23
+console.log(decrementResult);
+
+//reminder
+const k = 18;
+const l = 24;
+function reminder(k, l) {
+  return l % k;
+}
+let reminderResult = reminder(k, l);
+console.log(reminderResult);
