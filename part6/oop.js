0cbdb29fbaf93965816d503267d502d26664e9a0
Wahab1437 <wahab.suit997@gmail.com>
2025-03-27 00:39:57 +0500

    objects and conctructor

--- part6/oop.js
+++ part6/oop.js
@@ -11,4 +11,26 @@ const user = {
 };
 
 // console.log(user.username);
-console.log(user.getUserDetails());
+// console.log(user.getUserDetails());
+
+// const promiseOne = new Promise()    // new keyword is constructor function
+
+function User(username, loginCount, isLoggedIn) {
+  this.username = username;
+  this.loginCount = loginCount;
+  this.isLoggedIn = isLoggedIn;
+  this.greeting = function () {
+    console.log(`Welcome back, ${this.username}`);
+  };
+
+  return this; // this is implicit return in constructor function
+}
+
+const user1 = new User("Abdul", 7, true);
+const user2 = new User("wahab", 8, true);
+console.log(user1.constructor);
+console.log(user2);
+
+// new keyword is used to create an object from a constructor function
+// new keyword creates an empty object
+// new keyword binds the empty object to the this keyword
