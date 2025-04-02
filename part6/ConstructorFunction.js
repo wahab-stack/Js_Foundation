6fcf0a587fbd44ed2faf9338ef21b0c7933ba128
Wahab1437 <wahab.suit997@gmail.com>
2025-04-02 13:39:43 +0500

    Constructor Function

--- part6/ConstructorFunction.js
+++ part6/ConstructorFunction.js
@@ -14,7 +14,42 @@ function Car(make, model) {
 }
 
 let myCar = new Car("Toyota", "camry");
-console.log(myCar);
+// console.log(myCar);
 
 let myNewCar = new Car("Suzuki", "Mehran");
-console.log(myNewCar);
+// console.log(myNewCar);
+
+let myNewCars = new Car("Suzuki", "Alto");
+// console.log(myNewCars);
+
+function Tea(type) {
+  this.type = type;
+  this.describe = function () {
+    return `This is a cup of ${this.type}`;
+  };
+}
+
+let leamonTea = new Tea("leamon tea");
+// console.log(leamonTea.describe());
+
+function Animal(species) {
+  this.species = species;
+}
+Animal.prototype.sound = function () {
+  return `${this.species} makes a sound`;
+};
+
+let dog = new Animal("Dog");
+// console.log(dog.sound());
+
+let cat = new Animal("Cat");
+// console.log(cat.sound());
+
+function Drink(name) {
+  if (!new.target) {
+    throw new Error("Drink must be called with new keyword");
+  }
+  this.name = name;
+}
+let tea = new Drink("tea");
+let Coffee = new Drink("Coffee");
