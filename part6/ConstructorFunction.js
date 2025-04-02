c64d2f825abb5ec179f115af091761fd11c2cc03
Wahab1437 <wahab.suit997@gmail.com>
2025-04-02 11:45:19 +0500

    constructor Function

--- part6/ConstructorFunction.js
+++ part6/ConstructorFunction.js
@@ -1,1 +1,20 @@
-function greet() {}
+// function greet(name) {
+//     console.log("hello", name);
+
+// }
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
+let myCar = new Car("Toyota", "camry");
+console.log(myCar);
+
+let myNewCar = new Car("Suzuki", "Mehran");
+console.log(myNewCar);
