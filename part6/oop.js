4ba48656248b62ae354452bdcbc71c0cc6f40c96
Wahab1437 <wahab.suit997@gmail.com>
2025-04-06 12:04:37 +0500

    oop inheritance object-oriented concept

--- part6/oop.js
+++ part6/oop.js
@@ -50,4 +50,46 @@ function person(name, age) {
   this.age = age;
 }
 let john = new person("john doe", 20);
-console.log(john.name);
+// console.log(john.name);
+
+function Animal(type) {
+  this.type = type;
+}
+Animal.prototype.speak = function () {
+  return `${this.type} makes a sound`;
+};
+
+Array.prototype.wahab = function () {
+  return `Custom method ${this} `;
+};
+
+let myArray = [1, 2, 3];
+// console.log(myArray.wahab());
+
+let myNewArray = [1, 2, 3, 4, 5, 6];
+// console.log(myNewArray.wahab());
+
+// how classes created in javascript
+
+class vehicle {
+  constructor(make, model) {
+    this.make = make;
+    this.model = model;
+  }
+  start() {
+    return `${this.model} is a car from ${this.make} `;
+  }
+}
+
+class Car extends vehicle {
+  drive() {
+    return `${this.make} : This is an inheritance example `;
+  }
+}
+
+let myCar = new Car("Toyota", "Corolla");
+// console.log(myCar.start());
+// console.log(myCar.drive());
+
+let vehOne = new vehicle("Toyota", "Corolla");
+// console.log(vehOne.make);
