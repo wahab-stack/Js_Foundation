1bc49a4509d786cba73ec4798384c6deea3506c0
Wahab1437 <wahab.suit997@gmail.com>
2025-04-13 23:31:36 +0500

    DOM Challenges

--- part8/script.js
+++ part8/script.js
@@ -28,3 +28,24 @@ document.getElementById("changeOrder").addEventListener("click", function () {
 });
 
 //Example 4
+
+document.getElementById("addNewItem").addEventListener("click", function () {
+  let newItem = document.createElement("li");
+  newItem.textContent = "Eggs";
+  document.getElementById("shoppingList").appendChild(newItem);
+});
+
+//Example 5
+
+document
+  .getElementById("removeLastTask")
+  .addEventListener("click", function () {
+    let tasklist = document.getElementById("taskList");
+    tasklist.lastElementChild.remove();
+  });
+
+//Example 6
+
+document.getElementById("clickMeButton").addEventListener("click", function () {
+  alert("this DOM practice dude ");
+});
