3c2e4e5aaf8c57f01011c3910794aa7e110eef92
Wahab1437 <wahab.suit997@gmail.com>
2025-04-18 11:34:22 +0500

    Module import file

---
+++ Part9_adv/appM.js
@@ -0,0 +1,13 @@
+//Module import file
+
+//Default Import
+
+import multi from "./MathOperationsM.js";
+
+//named import
+
+import { add, sub } from "./MathOperationsM.js";
+
+console.log(multi(2, 3));
+console.log(add(2, 3));
+console.log(sub(5, 3));
